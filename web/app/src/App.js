1b573ba4a2c2e29d2af0fb4153a6210b8d72a483
Saffat Hasan <saffathasan@gmail.com>
2020-01-16 00:42:06 -0500

    refactor: moved hard-coded hex grid into SquareHexGrid component

--- web/app/src/App.js
+++ web/app/src/App.js
@@ -2,18 +2,26 @@ import React, {Component} from 'react';
 import {HexGrid, Layout, Hexagon, GridGenerator} from 'react-hexgrid';
 import './App.css';
 
-class App extends Component {
+class SquareHexGrid extends Component {
     render() {
         const hexagons = GridGenerator.orientedRectangle(4, 4);
 
         return (
+            <HexGrid width={1200} height={1000}>
+                <Layout size={{x: 7, y: 7}}>
+                    {hexagons.map((hex, i) => <Hexagon key={i} q={hex.q} r={hex.r} s={hex.s}/>)}
+                </Layout>
+            </HexGrid>
+        );
+    }
+}
+
+class App extends Component {
+    render() {
+        return (
             <div className="App">
                 <h1>Basic example of HexGrid usage.</h1>
-                <HexGrid width={1200} height={1000}>
-                    <Layout size={{x: 7, y: 7}}>
-                        {hexagons.map((hex, i) => <Hexagon key={i} q={hex.q} r={hex.r} s={hex.s}/>)}
-                    </Layout>
-                </HexGrid>
+                <SquareHexGrid />
             </div>
         );
     }
