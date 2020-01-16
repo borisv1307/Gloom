a1c3e04c13929552befc1eddbb1acbf66445597e
Saffat Hasan <saffathasan@gmail.com>
2020-01-16 01:08:26 -0500

    feat: added unit test for SquareHexGrid

--- web/app/src/App.test.js
+++ web/app/src/App.test.js
@@ -1,9 +1,10 @@
 import React from 'react';
 import { render } from '@testing-library/react';
-import App from './App';
+import SquareHexGrid from './App';
 
-test('renders learn react link', () => {
-  const { getByText } = render(<App />);
-  const linkElement = getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+test('renders square hex grid renders a square number of hexagons', () => {
+  const { container } = render(<SquareHexGrid />);
+  const numHexagons = container.querySelectorAll('.hexagon-group').length;
+  const isNumHexagonsAPerfectSquare = numHexagons > 0 && Math.sqrt(numHexagons) % 1 === 0;
+  expect(isNumHexagonsAPerfectSquare).toBe(true);
 });
