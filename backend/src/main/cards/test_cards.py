189d6ae3926e76faed8632c4656b552036122007
cameronbaffuto <cb3426@drexel.edu>
2020-01-30 16:18:44 -0500

    card logic v4

--- backend/src/main/cards/test_cards.py
+++ backend/src/main/cards/test_cards.py
@@ -9,7 +9,7 @@ def test_cards_starts_empty():
 def test_add_card_to_card_deck():
     cards = Cards()
     card_one = Card("card1")
-    cards.add(card_one)
+    cards.append(card_one)
     assert cards.cards.__len__() == 1
     assert cards.cards.__getitem__(0) == card_one
 
@@ -19,9 +19,9 @@ def test_remove_card_from_card_deck():
     card_one = Card("card1")
     card_two = Card("card2")
     card_three = Card("card3")
-    cards.add(card_one)
-    cards.add(card_two)
-    cards.add(card_three)
+    cards.append(card_one)
+    cards.append(card_two)
+    cards.append(card_three)
 
     cards.remove(card_two)
     assert cards.cards.__len__() == 2
