6db4fa9d9bb49d0bca99e1e6a837d6ab395623dc
Saffat Hasan <saffathasan@gmail.com>
2020-01-29 23:18:11 -0500

    feat: added select_first_room to dungeon generator

--- backend/src/main/dungeon/dungeon.py
+++ backend/src/main/dungeon/dungeon.py
@@ -1,9 +1,19 @@
 from backend.src.main.game.cutthroat import Cutthroat
 from backend.src.main.room.concrete_room_cards.den import Den
+from backend.src.main.room.constructed_room import ConstructedRoom
 
 
-class RandomDungeonGenerator:
+class RandomDungeonGenerator: # pylint: disable=too-few-public-methods
     def __init__(self):
         self.monster_cards = [Cutthroat() for _ in range(20)]
         self.room_cards = [Den() for _ in range(20)]
+        self.constructed_rooms = []
 
+    def select_first_room(self, random_wrapper):
+        chosen_monster_idx = random_wrapper.randrange(len(self.room_cards))
+        chosen_room_idx = random_wrapper.randrange(len(self.monster_cards))
+
+        chosen_monster = self.monster_cards.pop(chosen_monster_idx)
+        chosen_room = self.room_cards.pop(chosen_room_idx)
+
+        self.constructed_rooms.append(ConstructedRoom(chosen_room, chosen_monster))
--- backend/src/test/dungeon/test_dungeon.py
+++ backend/src/test/dungeon/test_dungeon.py
@@ -1,6 +1,10 @@
+from unittest.mock import MagicMock, call
+
 from backend.src.main.dungeon.dungeon import RandomDungeonGenerator
 from backend.src.main.game.random_monster_card import AbstractMonsterCard
+from backend.src.main.room.constructed_room import ConstructedRoom
 from backend.src.main.room.room import AbstractRoomCard
+from backend.src.main.wrappers.random_wrapper import RandomWrapper
 
 
 def test_dungeon_generator_has_20_monster_cards():
@@ -15,3 +19,42 @@ def test_dungeon_generator_has_20_room_cards():
     assert len(dungeon_generator.room_cards) == 20
     for item in dungeon_generator.room_cards:
         assert isinstance(item, AbstractRoomCard)
+
+
+def test_dungeon_generator_has_no_rooms_initially():
+    dungeon_generator = RandomDungeonGenerator()
+    assert not dungeon_generator.constructed_rooms
+
+
+def test_select_first_room_reduces_deck_sizes_by_one():
+    dungeon_generator = RandomDungeonGenerator()
+    dungeon_generator.select_first_room(RandomWrapper())
+    assert len(dungeon_generator.room_cards) == 19
+    assert len(dungeon_generator.monster_cards) == 19
+
+
+def test_select_first_room_adds_constructed_room():
+    dungeon_generator = RandomDungeonGenerator()
+    dungeon_generator.select_first_room(RandomWrapper())
+    assert len(dungeon_generator.constructed_rooms) == 1
+    assert isinstance(dungeon_generator.constructed_rooms[0], ConstructedRoom)
+
+
+def test_select_first_room_calls_random_choice_twice():
+    dungeon_generator = RandomDungeonGenerator()
+
+    # Note, we depend on the order of calls
+    mock = MagicMock()
+    mock.side_effect = [0, 0]
+
+    random_wrapper = RandomWrapper()
+    random_wrapper.randrange = mock
+
+    expected_room_argument = 20
+    expected_monster_argument = 20
+    expected_calls = [call(expected_room_argument), call(expected_monster_argument)]
+
+    dungeon_generator.select_first_room(random_wrapper)
+
+    mock.assert_has_calls(expected_calls)
+    assert mock.call_count == 2
