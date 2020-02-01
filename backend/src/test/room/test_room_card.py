64ffc5efff47a15ecde0c8063059005332c1f25f
Saffat Hasan <saffathasan@gmail.com>
2020-02-01 14:07:07 -0500

    fix: updated test_no_duplicated_character_number to use enum

--- backend/src/test/room/test_room_card.py
+++ backend/src/test/room/test_room_card.py
@@ -1,7 +1,7 @@
 
 import pytest
 
-from backend.src.main.game.values import DungeonCardValues
+from backend.src.main.game.values import DungeonCardValues, NumberedRoomTileValues
 from backend.src.main.room import room
 from backend.src.main.room.concrete_room_cards import den
 from backend.src.main.room.room_card_exceptions import DuplicateTileError
@@ -28,11 +28,12 @@ def test_can_have_multiple_empty_tiles(_test_room):
 
 
 def test_no_duplicate_character_number_in_room_tiles(_test_room):
-    _test_room.add_tile(1, 5, 13)
+    character_number = NumberedRoomTileValues.TWELVE
+    _test_room.add_tile(character_number, 5, 13)
     with pytest.raises(DuplicateTileError):
         # Both tiles have character_number 1
         # this is not valid
-        _test_room.add_tile(1, 5, 14)
+        _test_room.add_tile(character_number, 5, 14)
 
 
 def test_no_duplicate_coordinates_in_room(_test_room):
