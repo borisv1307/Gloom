3aa27839b04022827a77a319272dcdc6ec54610d
cameronbaffuto <cb3426@drexel.edu>
2020-01-31 19:15:02 -0500

    feat: added check for tile comparison

--- backend/src/main/room/tile.py
+++ backend/src/main/room/tile.py
@@ -12,3 +12,10 @@ class Tile:  # pylint: disable=too-few-public-methods
 
     def get_character_number(self):
         return self.character_number
+
+    def __eq__(self, other):
+        if not isinstance(other, Tile):
+            return False
+        return other.y_coordinate == self.y_coordinate and \
+            other.x_coordinate == self.x_coordinate and \
+            other.character_number == self.character_number
---
+++ backend/src/test/room/test_tile.py
@@ -0,0 +1,22 @@
+from backend.src.main.room.tile import Tile
+
+
+def test_tile_with_same_attributes_are_equal():
+    tile_one = Tile(0, 1, None)
+    tile_two = Tile(0, 1, None)
+    assert tile_one == tile_two
+
+
+def test_tile_and_dog_object_are_not_equal():
+    tile = Tile(0, 0, None)
+
+    class Dog:
+        pass
+
+    assert Dog() != tile
+
+
+def test_tiles_with_different_attributes_are_not_equal():
+    tile_one = Tile(1, 2, 3)
+    tile_two = Tile(4, 5, 6)
+    assert tile_one != tile_two
