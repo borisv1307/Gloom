79727e80180fd0c855f9275293b1ce338ebf8cbc
cameronbaffuto <cb3426@drexel.edu>
2020-02-07 18:41:48 -0500

    playable card logic

---
+++ backend/src/main/cards/test_playable_card_logic.py
@@ -0,0 +1,100 @@
+import pytest
+from backend.src.main.cards.card import Card
+from backend.src.main.cards.playable_card_logic import PlayableCardLogic
+
+
+@pytest.fixture(name='card')
+def create_card():
+    card = Card("1")
+    return card
+
+
+@pytest.fixture(name='playable_card_logic')
+def create_playable_card_logic(card):
+    hand = [card]
+    playable_card_logic = PlayableCardLogic(hand)
+    return playable_card_logic
+
+
+def test_starts_with_hand(playable_card_logic):
+    assert playable_card_logic.hand.__len__() == 1
+
+
+def test_discard_starts_empty(playable_card_logic):
+    assert playable_card_logic.discard.__len__() == 0
+
+
+def test_lost_starts_empty(playable_card_logic):
+    assert playable_card_logic.lost.__len__() == 0
+
+
+def test_hand_moves_to_discard(playable_card_logic, card):
+    playable_card_logic.hand_to_discard("1")
+    assert playable_card_logic.hand.__len__() == 0
+    assert playable_card_logic.discard.__len__() == 1
+    assert playable_card_logic.discard.__getitem__(0) == card
+
+
+def test_lost_moves_to_discard(playable_card_logic, card):
+    playable_card_logic.hand_to_lost("1")
+    playable_card_logic.lost_to_discard("1")
+    assert playable_card_logic.lost.__len__() == 0
+    assert playable_card_logic.discard.__len__() == 1
+    assert playable_card_logic.discard.__getitem__(0) == card
+
+
+def test_hand_moves_to_discard_with_many_cards():
+    card_one = Card("1")
+    card_two = Card("2")
+    card_three = Card("3")
+    card_four = Card("4")
+    card_five = Card("5")
+    hand = [card_one, card_two, card_three, card_four, card_five]
+    playable_card_logic = PlayableCardLogic(hand)
+    playable_card_logic.hand_to_discard("3")
+    assert playable_card_logic.hand.__len__() == 4
+    if playable_card_logic.hand == card_three:
+        assert False
+    assert playable_card_logic.discard.__len__() == 1
+    assert playable_card_logic.discard.__getitem__(0) == card_three
+
+
+def test_discard_moves_to_lost(playable_card_logic, card):
+    playable_card_logic.hand_to_discard("1")
+    playable_card_logic.discard_to_lost("1")
+    assert playable_card_logic.discard.__len__() == 0
+    assert playable_card_logic.lost.__len__() == 1
+    assert playable_card_logic.lost.__getitem__(0) == card
+
+
+def test_discard_moves_to_hand():
+    discard = []
+    card = Card("1")
+    discard.append(card)
+    playable_card_logic = PlayableCardLogic(discard)
+    playable_card_logic.discard_to_hand("1")
+    assert playable_card_logic.discard.__len__() == 0
+    assert playable_card_logic.hand.__len__() == 1
+    assert playable_card_logic.hand.__getitem__(0) == card
+
+
+def test_lost_moves_to_hand():
+    lost = []
+    card = Card("3")
+    lost.append(card)
+    playable_card_logic = PlayableCardLogic(lost)
+    playable_card_logic.lost_to_hand("3")
+    assert playable_card_logic.lost.__len__() == 0
+    assert playable_card_logic.hand.__len__() == 1
+    assert playable_card_logic.hand.__getitem__(0) == card
+
+
+def test_hand_moves_to_lost():
+    hand = []
+    playable_card_logic = PlayableCardLogic(hand)
+    card = Card("4")
+    hand.append(card)
+    playable_card_logic.hand_to_lost("4")
+    assert playable_card_logic.hand.__len__() == 0
+    assert playable_card_logic.lost.__len__() == 1
+    assert playable_card_logic.lost.__getitem__(0) == card
