e80b6b0ad35df2bf080c6256ab99bce75206e927
Saffat Hasan <saffathasan@gmail.com>
2020-02-12 17:33:28 -0500

    refactor: reduced code duplication in playable_card_logic

--- backend/src/main/game/cards/playable_card_logic.py
+++ backend/src/main/game/cards/playable_card_logic.py
@@ -1,4 +1,3 @@
-
 class PlayableCardLogic:
     def __init__(self, hand):
         self.hand = hand
@@ -8,43 +7,34 @@ class PlayableCardLogic:
             raise ValueError('non card type')
 
     def hand_to_discard(self, card_name):
-        for card in self.hand:
-            if card.name == card_name:
-                self.hand.remove(card)
-                self.discard.append(card)
-                break
+        self.move_card_by_name(card_name, self.hand, self.discard)
 
     def hand_to_lost(self, card_name):
-        for card in self.hand:
-            if card.name == card_name:
-                self.hand.remove(card)
-                self.lost.append(card)
-                break
+        self.move_card_by_name(card_name, self.hand, self.lost)
 
     def discard_to_lost(self, card_name):
-        for card in self.discard:
-            if card.name == card_name:
-                self.discard.remove(card)
-                self.lost.append(card)
-                break
+        self.move_card_by_name(card_name, self.discard, self.lost)
 
     def discard_to_hand(self, card_name):
-        for card in self.discard:
-            if card.name == card_name:
-                self.discard.remove(card)
-                self.hand.append(card)
-                break
+        self.move_card_by_name(card_name, self.discard, self.hand)
 
     def lost_to_discard(self, card_name):
-        for card in self.lost:
-            if card.name == card_name:
-                self.lost.remove(card)
-                self.discard.append(card)
-                break
+        self.move_card_by_name(card_name, self.lost, self.discard)
 
     def lost_to_hand(self, card_name):
-        for card in self.lost:
-            if card.name == card_name:
-                self.lost.remove(card)
-                self.hand.append(card)
+        self.move_card_by_name(card_name, self.lost, self.hand)
+
+    def move_card_by_name(self, card_name, source, destination):
+        for card in source:
+            if self.does_card_have_name(card, card_name):
+                self.move_card(card, source, destination)
                 break
+
+    @staticmethod
+    def does_card_have_name(card, name):
+        return card.name == name
+
+    @staticmethod
+    def move_card(card, source, destination):
+        source.remove(card)
+        destination.append(card)
