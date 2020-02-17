095b5bbbc2e6aae8e83d14fc53a5e3b823474bac
Saffat Hasan <saffathasan@gmail.com>
2020-02-17 01:39:54 -0500

    feat: added random_select to attack-modifiers

--- web/app/src/AttackModifier/AttackModifier.js
+++ web/app/src/AttackModifier/AttackModifier.js
@@ -33,6 +33,7 @@ class AttackModifier extends Component {
         super(props);
         this.state = {
             "deck": [1, 1, 0, 2],
+            "selected": [],
             "discard": [1, 2, -1, -2]
         }
     }
@@ -41,17 +42,51 @@ class AttackModifier extends Component {
         return (
             <div>
                 <Deck name="discard" cards={this.state.discard} hidden={false}/>
+                <Deck name="selected" cards={this.state.selected} hidden={false}/>
                 <Deck name="deck" cards={this.state.deck} hidden={true}/>
+                <button onClick={() => this.randomSelect()}>Random Select</button>
                 <button onClick={() => this.reset()}>Shuffle</button>
             </div>
         )
     }
 
-    reset() {
-        const new_deck = this.state.deck.concat(this.state.discard);
+    randomSelect() {
+        if (this.state.deck.length === 0) {
+            return;
+        }
+        const random_idx = this.randomIndex(this.state.deck);
+        const item = this.state.deck[random_idx];
+        const new_discard = this.state.discard.concat(this.state.selected);
+        const new_selected = [item];
+        const new_deck = this.removeByIndex(this.state.deck, random_idx)
+
         this.setState({
             "deck": new_deck,
-            "discard": []
+            "discard": new_discard,
+            "selected": new_selected
+        })
+
+    }
+
+    removeByIndex(array, index) {
+        return array.filter(function (el, i) {
+            return index !== i;
+        });
+    }
+
+    randomIndex(array) {
+        return Math.floor(Math.random() * array.length);
+    }
+
+    reset() {
+        const new_deck = this.state.deck
+            .concat(this.state.discard)
+            .concat(this.state.selected);
+
+        this.setState({
+            "deck": new_deck,
+            "discard": [],
+            "selected": []
         })
     }
 }
