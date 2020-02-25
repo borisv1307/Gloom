9ed4ce723961a55a31b1bd4a904ea42b4c9db0b6
Saffat Hasan <saffathasan@gmail.com>
2020-02-24 23:18:49 -0500

    feat: added concrete monster enums

--- backend/src/main/game/monster/values.py
+++ backend/src/main/game/monster/values.py
@@ -2,7 +2,6 @@ from enum import Enum
 
 
 class DungeonCardValues(Enum):
-    MONSTER = 'monster'
     CHARACTER = 'character'
     WALL = 'wall'
     OBSTACLE = 'obstacle'
@@ -13,6 +12,43 @@ class DungeonCardValues(Enum):
     EMPTY = 'empty'
     COIN = 'coin'
     TREASURE = '5 gold'
+
+
+class MonsterValues(Enum):
+    ANCIENT_ARTILLERY = 'ancient artillery'
+    BANDIT_ARCHER = 'bandit archer'
+    BANDIT_GUARD = 'bandit guard'
+    BLACK_IMP = 'black imp'
+    CAVE_BEAR = 'cave bear'
+    CITY_ARCHER = 'city archer'
+    CITY_GUARD = 'city guard'
+    CULTIST = 'cultist'
+    DEEP_TERROR = 'deep terror'
+    EARTH_DEMON = 'earth demon'
+    FLAME_DEMON = 'flame demon'
+    FROST_DEMON = 'frost demon'
+    FOREST_IMP = 'forest imp'
+    GIANT_VIPER = 'giant viper'
+    HARROWER_INFESTER = 'harrower infester'
+    HOUND = 'hound'
+    INOX_ARCHER = 'inox archer'
+    INOX_GUARD = 'inox guard'
+    INOX_SHAMAN = 'inox shaman'
+    LIVING_BONES = 'living bones'
+    LIVING_CORPSE = 'living corpse'
+    LIVING_SPIRIT = 'living spirit'
+    LURKER = 'lurker'
+    OOZE = 'ooze'
+    NIGHT_DEMON = 'night demon'
+    RENDING_DRAKE = 'rending drake'
+    SAVVAS_ICESTORM = 'savvas icestorm'
+    SAVVAS_LAVAFLOW = 'savvas lavaflow'
+    SPITTING_DRAKE = 'spitting drake'
+    STONE_GOLEM = 'stone golem'
+    SUN_DEMON = 'sun demon'
+    VERMLING_SCOUT = 'vermling scout'
+    VERMLING_SHAMAN = 'vermling shaman'
+    WIND_DEMON = 'wind demon'
 
 
 class UniqueTileValues(Enum):
