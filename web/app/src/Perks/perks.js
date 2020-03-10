56ff602dfaf66f2241b4f7dcdd31f23323f567e9
Saffat Hasan <saffathasan@gmail.com>
2020-03-10 05:18:44 -0400

    feat(add-perks): add checkbox to perks

--- web/app/src/Perks/perks.js
+++ web/app/src/Perks/perks.js
@@ -10,7 +10,19 @@ class Perks extends Component {
             "character": HARDCODED_CHARACTER,
             "perks": PerksData[HARDCODED_CHARACTER]
         }
+        this.addCheckedToPerks();
         this.performActions();
+    }
+
+    addCheckedToPerks() {
+        for (let perk_id in this.state.perks) {
+            let current_perk = this.state.perks[perk_id];
+            for (let action_id in current_perk.actions) {
+                let action = current_perk.actions[action_id];
+                action.checked = false;
+                this.setState(this.state.perks)
+            }
+        }
     }
 
     performActions() {
@@ -40,15 +52,19 @@ class Perks extends Component {
     handleRemove(card_value) {
         const card_index = this.state.cards.indexOf(card_value);
         const new_cards = this.state.cards.splice(card_index, 1);
-        this.updateState(new_cards);
+        this.updateCardState(new_cards);
     }
 
     handleAdd(card_value) {
         const new_cards = this.state.cards.concat(card_value);
-        this.updateState(new_cards);
+        this.updateCardState(new_cards);
     }
 
-    updateState(new_cards) {
+    updatePerkState(new_perk_state) {
+        this.state.perks = new_perk_state;
+    }
+
+    updateCardState(new_cards) {
         this.state.cards = new_cards;
     }
 
@@ -56,12 +72,42 @@ class Perks extends Component {
     render() {
         return (
             <div>
+                <div>PERKS:</div>
+                <ul>
+                    {this.state.perks.map((perk, iter) => (
+                        <ToggleablePerk description={perk.description}/>
+                    ))}
+                </ul>
                 <div>CARDS:</div>
                 <ul>
                     {this.state.cards.map((card, iter) => (
                         <li key={iter}>{card}</li>
                     ))}
                 </ul>
+            </div>
+        );
+    }
+}
+
+class ToggleablePerk extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            "checked": this.props.checked,
+            "key": this.props.key,
+            "description": this.props.description
+        };
+    }
+
+    render() {
+        return (
+            <div>
+                <input type="checkbox"
+                       id={'checkbox-' + this.state.key}
+                       key={this.state.key}
+                       checked={this.state.checked}
+                />
+                <label for={'checkbox-' + this.state.key}>{this.state.description}</label>
             </div>
         );
     }
