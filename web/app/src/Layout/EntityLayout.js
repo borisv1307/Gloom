e931ec0193a7c154e37864a456e0f1b03529e6e7
Sam Prasad <samridhprasad@gmail.com>
2020-03-10 15:49:13 -0400

    feat: Concrete monsters are generated dynamically, drag and drop works on the legend hexes

--- web/app/src/Layout/EntityLayout.js
+++ web/app/src/Layout/EntityLayout.js
@@ -4,43 +4,16 @@ import monsters from '../monsters';
 import './EntityLayout.css';
 import axios from 'axios'
 
-const USER_SERVICE_URL = 'http://0.0.0.0:5000/start';
+const USER_SERVICE_URL = 'http://127.0.0.1:5000/start';
 
-const text = {
-    character: "Character",
-    monster: "Monster",
-    wall: "Wall",
-    obstacle: "Obstacle",
-    trap: "Trap",
-    empty: "Empty",
-    hazard: "Hazardous Terrain",
-    dangerous: "Dangerous Terrain",
-    entrance_a: "Entry A",
-    entrance_b: "Entry B",
-    "exit a": "Exit A",
-    "exit b": "Exit B",
-    coin: "Coin"
+const name_values = ["Character", "Monster", "Wall", "Obstacle", "Trap", "Empty", "Hazardous Terrain", "Dangerous Terrain"];
 
-};
-const image = {
-    character: "https://img.icons8.com/color/96/000000/morty-smith.png",
-    monster: "https://img.icons8.com/color/96/000000/monster-face.png",
-    wall: "https://img.icons8.com/color/96/000000/brick-wall.png",
-    obstacle: "https://img.icons8.com/color/96/000000/roadblock.png",
-    trap: "https://img.icons8.com/color/96/000000/naval-mine.png",
-    empty: "",
-    hazard: "https://img.icons8.com/color/96/000000/self-destruct-button--v1.png",
-    dangerous: "https://img.icons8.com/color/96/000000/error.png"
-};
-
-
-var name_values = ["Character", "Monster", "Wall", "Obstacle", "Trap", "Empty", "Hazardous Terrain", "Dangerous Terrain"];
 
 class EntityLayout extends Component {
     constructor(props) {
         super(props);
 
-        const hexagons = GridGenerator.parallelogram(-1, 0, -1, 2).map((hexagon, index) => {
+        const legend_hexagons = GridGenerator.parallelogram(-1, 0, -1, 2).map((hexagon, index) => {
                 const monster_key = name_values[index].toLowerCase();
                 return Object.assign({}, hexagon, {
                         text: name_values[index],
@@ -50,10 +23,18 @@ class EntityLayout extends Component {
             }
         );
 
-        const hexagons2 = GridGenerator.hexagon(1);
+
+        const game_hexagons = GridGenerator.hexagon(1).map((hexagon, index) => {
+            const monster_key = name_values[index].toLowerCase();
+            return Object.assign({}, hexagon, {
+                text: {},
+                image: {}
+            });
+        }
+        );
 
         this.state = {
-            hexagons, hexagons2, isFetching: false,
+            legend_hexagons, game_hexagons, isFetching: false,
             JSONdata: {
                 "0": {
                     "name": "Den",
@@ -66,9 +47,7 @@ class EntityLayout extends Component {
                         },
                     }
                 }
-            },
-            image: image,
-            text: text
+            }
         };
     }
 
@@ -82,6 +61,7 @@ class EntityLayout extends Component {
             .then(response => {
                 this.setState({JSONdata: response.data, isFetching: false})
                 // console.log("First room = " + response.data[0].name)
+                console.log(response.data)
             })
             .catch(e => {
                 this.setState({...this.state, isFetching: false});
@@ -92,9 +72,11 @@ class EntityLayout extends Component {
         const data = this.state.JSONdata;
         const room1 = data[0].tiles;
         const tile_keys = Object.keys(room1);
-        return tile_keys.map(i =>
+        const game_hexes = tile_keys.map(i =>
             this.generateHexagon(0, i)
-        )
+        );
+        this.state.game_hexagons = game_hexes;
+        return game_hexes;
     }
 
     generateHexagon(roomID, tileID) {
@@ -117,26 +99,69 @@ class EntityLayout extends Component {
             data={hex_data}
             onDragStart={(e, h) => this.onDragStart(e, h)}
             onDragEnd={(e, h, s) => this.onDragEnd(e, h, s)}
-            onDrop={(e, h, t) => this.onDrop(e, h, t)}
+            onDrop={(e, h, t) => this.onDropGameHexes(e, h, t)}
             onDragOver={(e, h) => this.onDragOver(e, h)}
         >
             <Text>{text}</Text>
             <Pattern id={tileID} link={image} size={{x: 5, y: 5}}/>
         </Hexagon>
-
     }
 
 
-    onDrop(event, source, targetProps) {
-        const {hexagons} = this.state;
-        const hexes = hexagons.map(hex => {
-            if (HexUtils.equals(source.state.hex, hex)) {
-                hex.image = targetProps.data.image;
-                hex.text = targetProps.data.text;
-            }
-            return hex;
-        });
-        this.setState({hexagons: hexes});
+    // onDrop(event, destination, source) {
+    //     const {game_hexagons} = this.state;
+    //     // console.log(game_hexagons);
+    //     // return;
+    //     const hexes = game_hexagons.map(hex => {
+    //         if (this.areHexesEqual(source.hex, hex)) {
+    //             console.log("Hexes are equal");
+    //             console.log(hex.props);
+    //             console.log(destination.props);
+    //             // console.log(hex);
+    //             // console.log(hex.props);
+    //             hex.image = source.hex.image;
+    //             hex.text = source.hex.text;
+    //         }
+    //         return hex;
+    //     });
+    //     console.log(hexes);
+    //     if (hexes) {
+    //         this.setState({game_hexagons: hexes});
+    //     }
+    // }
+
+    // Making a specific onDrop() to handle game hexes
+    onDropGameHexes(event, source, targetProps) {
+    const {game_hexagons} = this.state;
+    const hexes = game_hexagons.map(hex => {
+      // When hexagon is dropped on this hexagon, copy it's image and text
+      if (this.areHexesEqual(source, hex)) {
+        hex.image = targetProps.image;
+        hex.text = targetProps.text;
+      }
+      return hex;
+    });
+    this.setState({ game_hexagons: hexes });
+  }
+
+    areHexesEqual(source, destination) {
+        const source_props = source.props;
+        const destination_props = destination.props;
+        console.log("SOURCE PROPS:");
+        console.log(source_props);
+        console.log(destination_props);
+        console.log("DESTINATION PROPS:");
+        console.log(destination_props);
+        if (source_props.q !== destination_props.q) {
+            return false;
+        }
+        if (source_props.r !== destination_props.r) {
+            return false;
+        }
+        if (source_props.s !== destination_props.s) {
+            return false;
+        }
+        return true;
     }
 
     onDragStart(event, source) {
@@ -146,48 +171,72 @@ class EntityLayout extends Component {
     }
 
     onDragOver(event, source) {
-        const blockedHexes = this.state.hexagons.filter(h => h.blocked);
-        const blocked = blockedHexes.find(blockedHex => {
-            return HexUtils.equals(source.state.hex, blockedHex);
-        });
-        console.log(source.props);
-        const {text} = source.props;
-
-        if (!blocked && !text) {
+        if (this.isValidDroppable(event, source)) {
             event.preventDefault();
         }
+    }
+
+    // Specifically for legend_hexagons
+    onDrop(event, source, targetProps) {
+        const {legend_hexagons} = this.state;
+        const hexes = legend_hexagons.map(hex => {
+          // When hexagon is dropped on this hexagon, copy it's image and text
+          if (HexUtils.equals(source.state.hex, hex)) {
+            hex.image = targetProps.data.image;
+            hex.text = targetProps.data.text;
+          }
+          return hex;
+        });
+        this.setState({ legend_hexagons: hexes });
+    }
+
+    isValidDroppable(event, source) {
+        return true;
+        // const blockedHexes = this.state.hexagons.filter(h => h.blocked);
+        // const blocked = blockedHexes.find(blockedHex => {
+        //     return HexUtils.equals(source.state.hex, blockedHex);
+        // });
+        // console.log(source.props);
+        // const {text} = source.props;
+        // console.log(text);
+        // return !blocked;
     }
 
     onDragEnd(event, source, success) {
         if (!success) {
             return;
         }
-        const {hexagons} = this.state;
+        const {game_hexagons} = this.state;
 
-        const hexes = hexagons.map(hex => {
+        const hexes = game_hexagons.map(hex => {
             if (HexUtils.equals(source.state.hex, hex)) {
                 hex.text = null;
                 hex.image = null;
             }
             return hex;
         });
-        this.setState({hexagons: hexes});
+        this.setState({game_hexagons: hexes});
     }
 
     render() {
-        const {hexagons} = this.state;
-        const gameHexes = this.parseHexagons();
+        const {legend_hexagons} = this.state;
+        const game_hexes = this.parseHexagons();
+        console.log("Game Hexagons (left)");
+        console.log(game_hexes);
+        console.log("Legend Hexagons (right)");
+        console.log(legend_hexagons);
+
         // map function needs to somehow have a sense of what {hexagons} state is and append the newly generated hexagon
         return (
             <HexGrid width={1300} height={500} viewBox="-50 -50 100 100">
-                <Layout className="game" size={{x: 5, y: 5}} flat={true} spacing={1.0} origin={{x: -30, y: 0}}>
+                <Layout className="game" size={{x: 6, y: 6}} flat={true} spacing={1.0} origin={{x: -30, y: 0}}>
                     {
-                        gameHexes
+                        game_hexes
                     }
                 </Layout>
-                <Layout className="tiles" size={{x: 10, y: 10}} flat={true} spacing={1.0} origin={{x: 60, y: -10}}>
+                <Layout className="tiles" size={{x: 6, y: 6}} flat={true} spacing={1.0} origin={{x: 60, y: -10}}>
                     {
-                        hexagons.map((hex, i) => (
+                        legend_hexagons.map((hex, i) => (
                             <Hexagon
                                 key={i}
                                 q={hex.q}
